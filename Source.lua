2fc2e8ec11dff34a2dac626be826b5b20eef0ca3
xplue31 <122674361+xplue31@users.noreply.github.com>
2023-01-20 22:59:29 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,1 +1,9 @@
-local Assets = game:GetObjects("rbxassetid://EnterIDHere")[1]
+local RunService = game:GetService("RunService")
+local TweenService = game:GetService("TweenService")
+local UserInputService = game:GetService("UserInputService")
+
+
+local assets = game:GetObjects("rbxassetid://12211969190")[1]
+local library = {}
+
+return library
